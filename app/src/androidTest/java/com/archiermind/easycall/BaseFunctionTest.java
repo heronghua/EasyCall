0404ebcb0faaf487f7e9f96628bd88ce5f9708af
heronghua <heronghua1989@126.com>
2021-04-24 10:09:24 +0800

    complete test case

--- app/src/androidTest/java/com/archiermind/easycall/BaseFunctionTest.java
+++ app/src/androidTest/java/com/archiermind/easycall/BaseFunctionTest.java
@@ -3,15 +3,26 @@ package com.archiermind.easycall;
 import android.app.Instrumentation;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Rect;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 
+import java.io.File;
+import java.io.IOException;
+
 @RunWith(AndroidJUnit4.class)
 public class BaseFunctionTest {
+
+    protected static final boolean DEBUG = true;
+
+    protected String TAG;
 
     protected UiDevice mUiDevice;
     protected Instrumentation mInstrumentation;
@@ -29,12 +40,20 @@ public class BaseFunctionTest {
         Intent intent = mContext.getPackageManager().getLaunchIntentForPackage(mTargetContext.getPackageName());
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
         mContext.startActivity(intent);
+
+        TAG = this.getClass().getSimpleName();
     }
 
+    /**
+     *
+     * @param selector  the object selector which you want to find on this page
+     * @param seconds seconds during check
+     * @return true if you find selector on current page
+     */
     protected boolean findUiObjectDuring(UiSelector selector,int seconds){
         long startTime = System.currentTimeMillis();
-        while (System.currentTimeMillis() < startTime + seconds * 1000){
-            if (selector != null){
+        while (System.currentTimeMillis() <= startTime + seconds * 1000){
+            if (mUiDevice.findObject(selector).exists()){
                 return true;
             }
             try {
@@ -43,7 +62,29 @@ public class BaseFunctionTest {
                 e.printStackTrace();
             }
         }
+
+        // could not find object during #seconds , we should dump hierarchy to check
+        try {
+            mUiDevice.dumpWindowHierarchy(new File("/sdcard/AT_DUMP/"+selector.hashCode()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return false;
     }
+
+    @After
+    public void tearDown(){
+        mUiDevice.pressBack();
+    }
+
+    /** Provide an long press method for uiObject*/
+	public void longPress(UiSelector selector) throws UiObjectNotFoundException {
+        UiObject uiObject = mUiDevice.findObject(selector);
+        Rect bounds = uiObject.getBounds();
+        int x = bounds.centerX();
+        int y = bounds.centerY();
+        mUiDevice.swipe(x, y, x, y, 300);
+    }
+
 
 }
--- app/src/androidTest/java/com/archiermind/easycall/BasicFunctionInstrumentedTest.java
+++ app/src/androidTest/java/com/archiermind/easycall/BasicFunctionInstrumentedTest.java
@@ -1,9 +1,13 @@
 package com.archiermind.easycall;
 
-import androidx.test.uiautomator.UiObject;
-import androidx.test.uiautomator.UiObjectNotFoundException;
-import androidx.test.uiautomator.UiSelector;
+import android.graphics.Rect;
+import android.util.Log;
+import androidx.test.uiautomator.*;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
@@ -12,21 +16,77 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class BasicFunctionInstrumentedTest extends BaseFunctionTest{
 
+    /** The phone num you want to type for test .*/
+    private static final String TEMP_PHONE_NUM1 = "987654321";
+    private static final String TEMP_PHONE_NUM2 = "9876543219";
+    private String CALLER_PKG_NAME = "com.android.incallui";;
+
     @Test
-    public void testAddContact() throws UiObjectNotFoundException {
-
+    public void testAddContactByCamera() throws IOException, UiObjectNotFoundException {
         UiObject grid = mUiDevice.findObject(new UiSelector().description("grid"));
-        //TODO how to perform double click
-        grid.click();
-        grid.clickAndWaitForNewWindow();
+        Rect bounds = grid.getBounds();
+        mUiDevice.executeShellCommand("input tap " + bounds.right /2+ " " + bounds.bottom/2);
+        mUiDevice.executeShellCommand("input tap " + bounds.right/2 + " " + bounds.bottom/2);
+        Log.i(TAG,"[testAddContact] bounds.right/2 " + bounds.right/2);
+        boolean enterNextPageSuccess = findUiObjectDuring(new UiSelector().text("获取照片"), 1);
+        assertTrue(enterNextPageSuccess);
 
+        //click take picture
+        UiObject takePictureObject = mUiDevice.findObject(new UiSelector().text("拍照"));
+        takePictureObject.clickAndWaitForNewWindow();
+
+        mUiDevice.executeShellCommand("input keyevent KEYCODE_CAMERA");
+        UiObject doneButtonUiObject = mUiDevice.findObject(new UiSelector().resourceIdMatches(".+done_button"));
+        doneButtonUiObject.click();
+
+        cropFlowCheck(TEMP_PHONE_NUM1);
     }
 
     @Test
-    public void testPhoneCall() throws UiObjectNotFoundException {
+    public void testAddContactByPicture() throws IOException, UiObjectNotFoundException {
+        UiObject grid = mUiDevice.findObject(new UiSelector().description("grid"));
+        Rect bounds = grid.getBounds();
+        mUiDevice.executeShellCommand("input tap " + bounds.right /2+ " " + bounds.bottom/2);
+        mUiDevice.executeShellCommand("input tap " + bounds.right/2 + " " + bounds.bottom/2);
+        Log.i(TAG,"[testAddContact] bounds.right/2 " + bounds.right/2);
+        boolean enterNextPageSuccess = findUiObjectDuring(new UiSelector().text("获取照片"), 1);
+        assertTrue(enterNextPageSuccess);
+
+        UiObject choosePictureObject = mUiDevice.findObject(new UiSelector().text("相册"));
+        choosePictureObject.clickAndWaitForNewWindow();
+
+        UiObject cameraObj = mUiDevice.findObject(new UiSelector().text("相机"));
+        cameraObj.clickAndWaitForNewWindow();
+
+        UiObject firstJpg = mUiDevice.findObject(new UiSelector().classNameMatches(".+ImageView$"));
+        firstJpg.clickAndWaitForNewWindow();
+
+        UiObject selectRightButtonUiObject = mUiDevice.findObject(new UiSelector().resourceIdMatches(".+head_select_right"));
+        selectRightButtonUiObject.clickAndWaitForNewWindow();
+
+        cropFlowCheck(TEMP_PHONE_NUM2);
+
+    }
+
+    private void cropFlowCheck(String phoneNum) throws UiObjectNotFoundException {
+        //check crop page is shown  com.android.gallery3d:id/head_select_right
+        UiObject selectRightButtonUiObject = mUiDevice.findObject(new UiSelector().resourceIdMatches(".+head_select_right"));
+        selectRightButtonUiObject.clickAndWaitForNewWindow();
+
+        UiObject numInputUiObject = mUiDevice.findObject(new UiSelector().description("inputNum"));
+        numInputUiObject.setText(phoneNum);
+
+        UiObject confirmUiObject = mUiDevice.findObject(new UiSelector().text("确定"));
+        confirmUiObject.clickAndWaitForNewWindow();
+
+        assertTrue(findUiObjectDuring(new UiSelector().text(phoneNum),2));
+    }
+
+    @Test
+    public void testCall() throws UiObjectNotFoundException, IOException {
         UiObject grid = mUiDevice.findObject(new UiSelector().description("grid"));
         UiSelector phoneNumItemSelector = new UiSelector().index(0);
         UiObject itemUiObject = grid.getChild(phoneNumItemSelector);
@@ -38,7 +98,26 @@ public class BasicFunctionInstrumentedTest extends BaseFunctionTest{
 
         itemUiObject.clickAndWaitForNewWindow();
 
-        assertTrue(findUiObjectDuring(new UiSelector().text(phoneNum),5));
+        String currentPackageName = mUiDevice.getCurrentPackageName();
+        Log.i(TAG,"testPhoneCall pkgName" + currentPackageName);
+
+        assertEquals(CALLER_PKG_NAME, currentPackageName);
+        mUiDevice.executeShellCommand("am force-stop " + currentPackageName);
+
+
+
+    }
+
+    @Test
+    public void testDeletePhoneNum() throws UiObjectNotFoundException {
+
+        UiSelector phoneNumSelector = new UiSelector().text(TEMP_PHONE_NUM1);
+        longPress(phoneNumSelector);
+
+        UiObject confirmUiObject = mUiDevice.findObject(new UiSelector().text("确定"));
+        confirmUiObject.clickAndWaitForNewWindow();
+
+        assertFalse(findUiObjectDuring(phoneNumSelector,5));
 
     }
 
